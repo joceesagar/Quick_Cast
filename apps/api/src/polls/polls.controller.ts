5586fa54b7ef7baac1d6f665a62c4b3b90897c38
joceesagar <joshisagarm2@gmail.com>
2025-12-29 21:41:03 +0545

    added create and join dto

--- apps/api/src/polls/polls.controller.ts
+++ apps/api/src/polls/polls.controller.ts
@@ -1,15 +1,18 @@
-import { Controller, Get, Logger, Post } from '@nestjs/common';
+import { Body, Controller, Get, Logger, Post } from '@nestjs/common';
+import { CreatePollDto, JoinPollDto } from './dtos';
 
 @Controller('polls')
 export class PollsController {
     @Post()
-    async create() {
-        Logger.log('In create!')
+    async create(@Body() createPollDto: CreatePollDto) {
+        Logger.log('In create!');
+        return createPollDto
     }
 
     @Post('/join')
-    async join() {
-        Logger.log("Joined")
+    async join(@Body() JoinPollDto: JoinPollDto) {
+        Logger.log("In Join!")
+        return JoinPollDto
     }
 
     @Post('/rejoin')
